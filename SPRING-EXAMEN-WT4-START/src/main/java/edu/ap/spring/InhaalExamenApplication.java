a34cb544e468a849c7667cb097eaa24192ee9d39
StijnDefour <defourstijn10@gmail.com>
2018-04-16 15:40:48 +0200

    post new inhaalexamen

--- SPRING-EXAMEN-WT4-START/src/main/java/edu/ap/spring/InhaalExamenApplication.java
+++ SPRING-EXAMEN-WT4-START/src/main/java/edu/ap/spring/InhaalExamenApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -18,31 +19,8 @@ import edu.ap.spring.controller.InhaalExamenController;
 import edu.ap.spring.redis.RedisService;
 
 @SpringBootApplication
+@ComponentScan("edu.ap.spring")
 public class InhaalExamenApplication {
-	private RedisService service;
-	private String CHANNEL = "edu:ap:spring";
-	private String KEY = "edu:ap:spring:test";
-	
-	@Autowired
-	public void setRedisService(RedisService service) {
-		this.service = service;
-	}
-	
-	@Bean
-	RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
-											MessageListenerAdapter listenerAdapter) {
-
-		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.addMessageListener(listenerAdapter, new ChannelTopic(CHANNEL));
-
-		return container;
-	}
-
-	@Bean
-	MessageListenerAdapter listenerAdapter(InhaalExamenController controller) {
-		return new MessageListenerAdapter(controller, "onMessage");
-	}
 	
 	@Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
--- SPRING-EXAMEN-WT4-START/src/main/java/edu/ap/spring/controller/InhaalExamenController.java
+++ SPRING-EXAMEN-WT4-START/src/main/java/edu/ap/spring/controller/InhaalExamenController.java
@@ -7,10 +7,17 @@ import java.util.Map;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.ap.spring.model.InhaalExamen;
@@ -18,7 +25,7 @@ import edu.ap.spring.redis.RedisService;
 
 @Controller
 public class InhaalExamenController {
-
+	
 	private List<String> redisMessages = new ArrayList<String>();
 	private RedisService service;
 	
@@ -27,12 +34,11 @@ public class InhaalExamenController {
 		this.service = service;
 	}
 	
-	@PostMapping("/new")
-	public String setNew(@RequestParam("student") String student, 
-			   				@RequestParam("exam") String exam,
-			   				@RequestParam("reason") String reason,
-			   				Model model) {
 	
+	/*public String setNew(@RequestParam("student") String student, 
+			   				@RequestParam("exam") String exam,
+			   				@RequestParam("reason") String reason) {
+		
 			InhaalExamen examen = new InhaalExamen(student, exam, reason);
 			String key = "inhaalexamens:"+examen.getStudent()+":"+examen.getExam()+":"+examen.getReason()+":"+examen.getDate();
 			
@@ -41,15 +47,24 @@ public class InhaalExamenController {
 			}	
 			
 			return "result";
+	}*/
+	
+	@RequestMapping(method = RequestMethod.POST, value="/new")
+	public String setNew(@RequestBody InhaalExamen input) {
+		
+		String key = "inhaalexamens:"+input.getStudent()+":"+input.getExam()+":"+input.getReason()+":"+input.getDate();
+		service.setKey(key, "1");
+		
+		return "result";
 	}
 	
 	@RequestMapping("/list")
-	public String setGrade(@RequestParam("student") String student,
-				Model model) {
+	public String setGrade(@RequestParam("student") String student) {
 
+		
 		String key = "inhaalexamens:"+student+":*";
 		
-		Set<String> inhaalexamens = service.keys("inhaalexamens:"+student+":*");	
+		Set<String> inhaalexamens = service.keys(key);	
 		
 		return "result";
 	}
