07ea0c7a05d8140624a91022976fa2b57e9f4ff8
StijnDefour <defourstijn10@gmail.com>
2018-04-16 16:05:49 +0200

    laatste changes

--- ExamenWebtech4/urlparts/urls.py
+++ ExamenWebtech4/urlparts/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(https?:\/\/)?([\da-z\.-]+\.[a-z\.]{2,6}|[\d\.]+)([\/:?=&#]{1}[\da-z\.-]+)*[\/\?]?$', views.index, name='index'),
+    url(r'^.*$', views.index, name='index'),
 ]
--- ExamenWebtech4/urlparts/views.py
+++ ExamenWebtech4/urlparts/views.py
@@ -2,9 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+import re
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    test = request.path
+    test = test.replace("urlparts", "----")
+    test = test.replace("/", "----")
+    return HttpResponse(test)
